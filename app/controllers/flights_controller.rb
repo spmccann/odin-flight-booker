15fd9e5eea5f7e1cedc1499dcc9038b20574961d
Scott <scottpmccann@gmail.com>
2022-08-29 21:51:54 -0700

    radio label clickable, query for day correct, form for select to new file

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,7 @@
 class FlightsController < ApplicationController
   def index
-     params[:date] = '10/10/2020' if params[:date].nil?
-    @matching_flights = Flight.all.where('departure_airport_id = ? AND arrival_airport_id = ? AND datetime LIKE ?',
-                                         Airport.find_by(code: params[:departure_code]), Airport.find_by(code: params[:arrival_code]), DateTime.parse(params[:date]))
+    params[:date] = '10/10/2020' if params[:date].nil?
+    @matching_flights = Flight.all.where('departure_airport_id = ? AND arrival_airport_id = ? AND datetime BETWEEN ? AND ?',
+                                         Airport.find_by(code: params[:departure_code]), Airport.find_by(code: params[:arrival_code]), DateTime.parse(params[:date]).beginning_of_day, DateTime.parse(params[:date]).end_of_day)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,6 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
 
   def flight_datetime_formatted
-    datetime.strftime('%m/%d/%Y')
+    datetime.strftime('%d/%m/%Y')
   end
 end
